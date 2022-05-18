74642eb8e0c6a83c818b3ce118777e8f98daf831
unknown <minhtu962003@gmail.com>
2022-05-18 20:28:00 +0700

    update hide scroll header

--- script.js
+++ script.js
@@ -18,4 +18,14 @@ var index = 1;
 		index = 0;
 	}
 } 
-	
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("header").style.top = "0";
+  } else {
+    document.getElementById("header").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
