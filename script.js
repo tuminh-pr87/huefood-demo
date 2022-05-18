fac9def46ec488ec4dd8a633d8c1619589acbd43
unknown <minhtu962003@gmail.com>
2022-05-18 18:09:36 +0700

    update all website

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 var index = 1;
-		changeImage = function(){
+		changeImage1 = function(){
 		var imgs = ["images/banner0.png","images/banner1.png"];
 		document.getElementById('banner-click').src = imgs[index];
 		index++;
@@ -8,3 +8,14 @@ var index = 1;
 			index = 0;
 		}
 	} 
+
+	var index = 1;
+	changeImage2 = function(){
+	var imgs = ["images/about-slide1.png","images/about-slide2.jpg","images/about-slide3.jpg"];
+	document.getElementById('info-click').src = imgs[index];
+	index++;
+	if (index == 3){
+		index = 0;
+	}
+} 
+	
