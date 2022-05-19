0925d64565f4de58825732816d1a53ab42c90576
unknown <minhtu962003@gmail.com>
2022-05-19 11:02:55 +0700

    update

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-
+// Chuyển slide của banner
 var index = 1;
-		changeImage1 = function(){
-		var imgs = ["images/banner0.png","images/banner1.png"];
-		document.getElementById('banner-click').src = imgs[index];
-		index++;
-		if (index == 2){
-			index = 0;
-		}
-	} 
-
-	var index = 1;
-	changeImage2 = function(){
-	var imgs = ["images/about-slide1.png","images/about-slide2.jpg","images/about-slide3.jpg"];
-	document.getElementById('info-click').src = imgs[index];
+changeImage1 = function(){
+	var imgs = ["images/banner0.png","images/banner1.png"];
+	document.getElementById('banner-click').src = imgs[index];
 	index++;
-	if (index == 3){
+	if (index == 2){
 		index = 0;
 	}
 } 
+// Chuyển slide của img .about
+var index = 1;
+	changeImage2 = function(){
+		var imgs = ["images/about-slide1.png","images/about-slide2.jpg","images/about-slide3.jpg"];
+		document.getElementById('info-click').src = imgs[index];
+		index++;
+		if (index == 3){
+			index = 0;
+		}
+	}
 
+// Ẩn logo-nav-icons
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
